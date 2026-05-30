c25ce747b6bd59b6785992309be50eb8d4ae83b4
flurolabacademy <flurolabacademy@gmail.com>
2026-05-30 00:38:36 -0500

    Update generate-titles.js

--- netlify/functions/generate-titles.js
+++ netlify/functions/generate-titles.js
@@ -17,7 +17,7 @@ exports.handler = async (event, context) => {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({
-        model: 'llama3-70b-8192',
+        model: 'llama-3.1-70b-versatile',
         messages: [
           {
             role: 'system',
